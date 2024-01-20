5dbba979ad9df3fd36ea59d25d076b25ea8da4d4
OSSU <o.ss.u2056@gmail.com>
2024-01-21 00:16:04 +0900

    feat: ReportDto 생성 - 게시글 신고 시 게시글 아이디와 신고 사유 필요

---
+++ src/main/java/com/softee5/mobil2team/dto/ReportDto.java
@@ -0,0 +1,13 @@
+package com.softee5.mobil2team.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReportDto {
+    private Long postId;
+    private String reason;
+}
